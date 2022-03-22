2c3e657887b986178a7520baff8c049ff7455590
Bob Glickstein <bob.glickstein@coinbase.com>
2022-03-21 21:18:56 -0700

    Make //go:build line match //+build line.

--- keyring_unix.go
+++ keyring_unix.go
@@ -1,5 +1,5 @@
-//go:build (dragonfly && cgo) || (freebsd && cgo) || linux || netbsd || openbsd
-// +build (dragonfly && cgo) || (freebsd && cgo) || linux || netbsd || openbsd
+//go:build dragonfly || freebsd || linux || netbsd || openbsd
+// +build dragonfly freebsd linux netbsd openbsd
 
 package keyring
 
